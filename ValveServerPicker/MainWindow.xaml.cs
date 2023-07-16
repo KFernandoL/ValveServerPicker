1cd98b6ccc98f77f0bad432ce8b3a4ee67959161
Edder Barillas <61389832+KAOSNox@users.noreply.github.com>
2023-07-16 15:46:57 -0600

    added titlebar and events buttons

--- ValveServerPicker/MainWindow.xaml.cs
+++ ValveServerPicker/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,28 @@ namespace ValveServerPicker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #region Custom titleBar actions
+        private void btnCerrar_MouseClick(object sender, RoutedEventArgs e) { Close(); }
+        private void btnMinimizar_MouseClick(object sender, RoutedEventArgs e) { WindowState = WindowState.Minimized; }
+        private void titleBar_MouseLeftButtonDown(object sender, MouseEventArgs e) { DragMove(); }
+        #endregion
+
+        #region Custom titleBar Hover
+        //Hover effect button Min
+        private void btnMinimizar_MouseEnter(object sender, MouseEventArgs e) { btnMinimizar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#b8aa02")); }
+        private void btnMinimizar_MouseLeave(object sender, MouseEventArgs e) { btnMinimizar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FAE800")); }
+        //Hover effect btn Closed
+        private void btnCerrar_MouseEnter(object sender, MouseEventArgs e) { btnCerrar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#96262a")); }
+
+        private void btnCerrar_MouseLeave(object sender, MouseEventArgs e) { btnCerrar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FC4850")); }
+        #endregion
+
     }
 }
