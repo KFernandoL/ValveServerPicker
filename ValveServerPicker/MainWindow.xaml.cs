3f34fdbf807fb8df59e36b85a9f084f3f1a4424f
Edder Barillas <61389832+KAOSNox@users.noreply.github.com>
2023-07-17 20:52:16 -0600

    added get servers, added buttons menu

--- ValveServerPicker/MainWindow.xaml.cs
+++ ValveServerPicker/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using ValveServerPicker.Models;
+using ValveServerPicker.Servicesasd;
 using ValveServerPicker.Tools;
 
 namespace ValveServerPicker
@@ -24,13 +25,30 @@ namespace ValveServerPicker
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+        public static int menuSelected = 0;
+        //img ico TF2
+        private readonly BitmapImage imgTF2Selected = new BitmapImage(new Uri("pack://application:,,,/img/tf2-select.png"));
+        private readonly BitmapImage imgTF2Unselected = new BitmapImage(new Uri("pack://application:,,,/img/tf2.png"));
+        //img ico CSGO
+        private readonly BitmapImage imgCSGOSelected = new BitmapImage(new Uri("pack://application:,,,/img/csgo-select.png"));
+        private readonly BitmapImage imgCSGOUnselected = new BitmapImage(new Uri("pack://application:,,,/img/csgo.png"));
+
         public MainWindow()
         {
             InitializeComponent();
+
+            LoadServers();
+        }
+
+        public async void LoadServers()
+        {
+            var serversTF2 = await SteamServersServices.getTF2Servers();
+            var CSGO = await SteamServersServices.getTF2Servers();
         }
 
         #region Custom titleBar actions
-        private void btnCerrar_MouseClick(object sender, RoutedEventArgs e) { Close(); }
+        private void btnCerrar_MouseClick(object sender, RoutedEventArgs e)
+        { Close(); }
         private void btnMinimizar_MouseClick(object sender, RoutedEventArgs e) { WindowState = WindowState.Minimized; }
         private void titleBar_MouseLeftButtonDown(object sender, MouseEventArgs e) { DragMove(); }
         #endregion
@@ -43,6 +61,48 @@ namespace ValveServerPicker
         private void btnCerrar_MouseEnter(object sender, MouseEventArgs e) { btnCerrar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#96262a")); }
 
         private void btnCerrar_MouseLeave(object sender, MouseEventArgs e) { btnCerrar.Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FC4850")); }
+        #endregion
+
+        #region efectos de hover y onclick en las opciones del menu
+        //Click izquierdo al icono de tf2 en el menu
+        private void MenuTF2_MouseClick(object sender, RoutedEventArgs e)
+        {
+            MenuTF2.Background = new ImageBrush { ImageSource = imgTF2Selected, TileMode = TileMode.None, Stretch = Stretch.Uniform };
+            MenuCSGO.Background = new ImageBrush { ImageSource = imgCSGOUnselected, TileMode = TileMode.None, Stretch = Stretch.Uniform };
+            // await OcularContenedor();
+            // menuOpcionSelect = 0;
+            // await ReglasActivas();
+            // await serversFila.UpdateSwitchs(serversBloqueados);
+            // await MostrarContenedor();
+        }
+
+
+        //Mouse encima del icono de tf2 en el menu
+        private void MenuTF2_MouseEnter(object sender, MouseEventArgs e) { MenuTF2.Background = new ImageBrush { ImageSource = imgTF2Selected, TileMode = TileMode.None, Stretch = Stretch.Uniform }; }
+
+        //Mouse sale del icono de tf2 en el menu
+        private void MenuTF2_MouseLeave(object sender, MouseEventArgs e) { MenuTF2.Background = menuSelected == 0 ? new ImageBrush { ImageSource = imgTF2Selected, TileMode = TileMode.None, Stretch = Stretch.Uniform } : new ImageBrush { ImageSource = imgTF2Unselected, TileMode = TileMode.None, Stretch = Stretch.Uniform }; }
+
+        //Click izquierdo al icono de csgo en el menu
+        private void MenuCSGO_MouseClick(object sender, RoutedEventArgs e)
+        {
+            /*  await OcularContenedor();
+              
+              menuOpcionSelect = 1;
+              await ReglasActivas();
+              await serversFila.UpdateSwitchs(serversBloqueados);
+              await MostrarContenedor();*/
+            MenuTF2.Background = new ImageBrush { ImageSource = imgTF2Unselected, TileMode = TileMode.None, Stretch = Stretch.Uniform };
+            MenuCSGO.Background = new ImageBrush { ImageSource = imgCSGOSelected, TileMode = TileMode.None, Stretch = Stretch.Uniform };
+        }
+
+
+        //Mouse encima del icono de csgo en el menu
+        private void MenuCSGO_MouseEnter(object sender, MouseEventArgs e) { MenuCSGO.Background = new ImageBrush { ImageSource = imgCSGOSelected, TileMode = TileMode.None, Stretch = Stretch.Uniform }; }
+
+        //Mouse sale del icono de csgo en el menu
+        private void MenuCSGO_MouseLeave(object sender, MouseEventArgs e) { MenuCSGO.Background = menuSelected == 1 ? new ImageBrush { ImageSource = imgCSGOSelected, TileMode = TileMode.None, Stretch = Stretch.Uniform } : new ImageBrush { ImageSource = imgCSGOUnselected, TileMode = TileMode.None, Stretch = Stretch.Uniform }; }
+
         #endregion
 
     }
--- ValveServerPicker/Models/UI/ServersApp.cs
+++ ValveServerPicker/Models/UI/ServersApp.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
 
-namespace ValveServerPicker.Models
+namespace ValveServerPicker.Models.UI
 {
     public class ServersApp
     {
